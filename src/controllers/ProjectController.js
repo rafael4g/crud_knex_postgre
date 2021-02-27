c07374c24a03962ce4d155deaf2667a27d9f6a8c
RAFAEL DIOGO DA SILVA <92049358@corp.clarobr>
2021-02-26 22:49:41 -0300

    rota de projects

---
+++ src/controllers/ProjectController.js
@@ -0,0 +1,25 @@
+const knex = require('../database');
+
+module.exports = {
+ async index(req, res, next) {
+   try {     
+    const { user_id } = req.query;
+
+    const query = knex('projects')
+    
+    if (user_id){
+      query
+        .where({user_id})
+        .join('users','users.id', '=', 'projects.user_id')
+        .select('projects.*', 'users.username')
+    }
+
+    const results = await query;
+
+    return res.json(results);
+
+   } catch (error) {
+     next(error)
+   }
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
+const ProjectController = require('./controllers/ProjectController');
 const routes = express.Router();
 
-routes.get('/users', UserController.index)
-routes.post('/users', UserController.create)
-routes.put('/users/:id', UserController.update)
-routes.delete('/users/:id', UserController.delete)
+routes
+  // Users
+  .get('/users', UserController.index)
+  .post('/users', UserController.create)
+  .put('/users/:id', UserController.update)
+  .delete('/users/:id', UserController.delete)
+  // Projects
+  .get('/projects', ProjectController.index)  
 
 module.exports = routes;
